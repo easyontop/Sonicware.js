57353eeba76cf5519b573e183d56ce8d4bc6f07b
abela46 <mes20150119@hotmail.com>
2023-07-12 21:57:16 +0800

    Typings didn't update lol

--- typings/src/canvas/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-interface gradientOptions {
-    start: string;
-    end: string;
-    width?: number;
-    height?: number;
-}
-export declare function gradient(options: gradientOptions): Buffer;
-export {};
--- typings/src/nqn.d.ts
+++
@@ -1,4 +0,0 @@
-export type nqnOptions = {
-    strict?: boolean;
-};
-export declare function nqn(message: any, options?: nqnOptions): Promise<void>;
